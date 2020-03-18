ae863ba7be932ed773a8362891e00a4a36ecc484
macnick <nick@macexperts.gr>
2020-03-18 09:59:41 +0200

    Add style and css loader to npm modules

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import '../node_modules/materialize-css/dist/css/materialize.min.css';
+import '../node_modules/materialize-css/dist/js/materialize.min.js';
+
+console.log('Here will be my content');
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  // plugins: [
+  //   new CleanWebpackPlugin(),
+  //   new HtmlWebpackPlugin({
+  //     title: 'Output Management',
+  //   }),
+  // ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+      //     {
+      //       test: /\.(png|svg|jpg|gif)$/,
+      //       use: [
+      //         'file-loader',
+      //       ],
+      //     },
+      //     {
+      //       test: /\.(woff|woff2|eot|ttf|otf)$/,
+      //       use: [
+      //         'file-loader',
+      //       ],
+      //     },
+      //     {
+      //       test: /\.(csv|tsv)$/,
+      //       use: [
+      //         'csv-loader',
+      //       ],
+      //     },
+      //     {
+      //       test: /\.xml$/,
+      //       use: [
+      //         'xml-loader',
+      //       ],
+      //     },
+      //     {
+      //       test: /\.m?js$/,
+      //       exclude: /(node_modules|bower_components)/,
+      //       use: {
+      //         loader: 'babel-loader',
+      //         options: {
+      //           presets: ['@babel/preset-env']
+      //         }
+      //       }
+      //     }
+    ]
+  }
+};
