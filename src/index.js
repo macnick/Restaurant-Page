b47a431fbbc042b955487a64cbe7f59c11499e8f
macnick <nick@macexperts.gr>
2020-03-19 18:04:05 +0200

    Add background image and proper CSS

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import '../node_modules/materialize-css/dist/css/materialize.min.css';
 import '../node_modules/materialize-css/dist/js/materialize.min.js';
 import './style.css';
 
-// document.addEventListener('DOMContentLoaded', function() {
-//   var elems = document.querySelectorAll('.dropdown-trigger');
-//   var instances = M.Dropdown.init(elems, options);
-// });
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown');
+  console.log(elems);
+  // var instances = M.Dropdown.init(elems, options);
+});
 
-console.log('Here will be my content');
+console.log('Here will be my content new');
--- webpack.config.js
+++ webpack.config.js
@@ -21,13 +21,11 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
       }
-      //     {
-      //       test: /\.(png|svg|jpg|gif)$/,
-      //       use: [
-      //         'file-loader',
-      //       ],
-      //     },
       //     {
       //       test: /\.(woff|woff2|eot|ttf|otf)$/,
       //       use: [
